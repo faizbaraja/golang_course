eb3a4d9bb05f8f8e1d95a405ecdb1983071fd3ad
faiz baraja <faiz.baraja89@gmail.com>
2022-11-22 17:21:12 +0700

    feat : modify json response using snake case

--- internal/app/domain/exercise.go
+++ internal/app/domain/exercise.go
@@ -3,33 +3,33 @@ package domain
 import "time"
 
 type Exercise struct {
-	ID          int
-	Title       string
-	Description string
-	Questions   []Question
+	ID          int        `json:"id"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	Questions   []Question `json:"questions"`
 }
 
 type Question struct {
-	ID            int
-	ExerciseID    int
-	Body          string
-	OptionA       string
-	OptionB       string
-	OptionC       string
-	OptionD       string
-	CorrectAnswer string
-	Score         int
-	CreatorID     int
-	CreatedAt     time.Time
-	UpdatedAt     time.Time
+	ID int `json:"id"`
+	// ExerciseID    int       `json:"exerciseID"`
+	Body    string `json:"body"`
+	OptionA string `json:"option_a"`
+	OptionB string `json:"option_b"`
+	OptionC string `json:"option_c"`
+	OptionD string `json:"option_d"`
+	// CorrectAnswer string    `json:"correctAnswer"`
+	Score int `json:"score"`
+	// CreatorID     int       `json:"creatorID"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"created_at"`
 }
 
 type Answer struct {
-	ID         int
-	ExerciseID int
-	QuestionID int
-	UserID     int
-	Answer     string
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
+	ID         int       `json:"id"`
+	ExerciseID int       `json:"exerciseID"`
+	QuestionID int       `json:"questionID"`
+	UserID     int       `json:"userID"`
+	Answer     string    `json:"answer"`
+	CreatedAt  time.Time `json:"createdAt"`
+	UpdatedAt  time.Time `json:"updatedAt"`
 }
